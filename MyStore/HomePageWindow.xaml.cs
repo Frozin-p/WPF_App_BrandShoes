8b3ede331605c6080c7d3688de4a8efa90415329
Frozin <nikitin278@bk.ru>
2023-12-14 22:38:53 +0300

    Реализован поиск на главной странице

--- MyStore/HomePageWindow.xaml.cs
+++ MyStore/HomePageWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,8 +19,10 @@ namespace MyStore
     /// <summary>
     /// Interaction logic for HomePageWindow.xaml
     /// </summary>
+    /// 
     public partial class HomePageWindow : Window
     {
+
         ApplicationContext datb;
         public HomePageWindow()
         {
@@ -69,7 +73,95 @@ namespace MyStore
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
+            var tb = sender as TextBox;
+            if (tb.Text.ToLower() == "ni" || tb.Text.ToLower() == "nik" || tb.Text.ToLower() == "nike")
+            {
+                DunkGrid.Visibility= Visibility.Visible;
 
+                SambaGrid.Visibility = Visibility.Collapsed;
+                RSZGrid.Visibility = Visibility.Collapsed;
+                OcaiGrid.Visibility = Visibility.Collapsed;
+                VansGrid.Visibility = Visibility.Collapsed;
+            }
+            else if (tb.Text.ToLower() == "n")
+            {
+                DunkGrid.Visibility = Visibility.Visible;
+                VansGrid.Visibility = Visibility.Visible;
+
+                SambaGrid.Visibility = Visibility.Collapsed;
+                RSZGrid.Visibility = Visibility.Collapsed;
+                OcaiGrid.Visibility = Visibility.Collapsed;
+            }
+            else if (tb.Text.ToLower() == "oc" || tb.Text.ToLower() == "oca" || tb.Text.ToLower() == "ocai")
+            {
+                OcaiGrid.Visibility = Visibility.Visible;
+
+                VansGrid.Visibility = Visibility.Collapsed;
+                DunkGrid.Visibility = Visibility.Collapsed;
+                SambaGrid.Visibility = Visibility.Collapsed;
+                RSZGrid.Visibility = Visibility.Collapsed;
+            }
+            else if (tb.Text.ToLower() == "o")
+            {
+                OcaiGrid.Visibility = Visibility.Visible;
+                VansGrid.Visibility = Visibility.Visible;
+
+                DunkGrid.Visibility = Visibility.Collapsed;
+                SambaGrid.Visibility = Visibility.Collapsed;
+                RSZGrid.Visibility = Visibility.Collapsed;
+            }
+            else if (tb.Text.ToLower() == "v" || tb.Text.ToLower() == "va" || tb.Text.ToLower() == "van" || tb.Text.ToLower() == "vans")
+            {
+                VansGrid.Visibility = Visibility.Visible;
+
+                DunkGrid.Visibility = Visibility.Collapsed;
+                SambaGrid.Visibility = Visibility.Collapsed;
+                RSZGrid.Visibility = Visibility.Collapsed;
+                OcaiGrid.Visibility = Visibility.Collapsed;
+            }
+            else if (tb.Text.ToLower() == "ad" || tb.Text.ToLower() == "adi" || tb.Text.ToLower() == "adid")
+            {
+                SambaGrid.Visibility = Visibility.Visible;
+
+                RSZGrid.Visibility = Visibility.Collapsed;
+                OcaiGrid.Visibility = Visibility.Collapsed;
+                VansGrid.Visibility = Visibility.Collapsed;
+                DunkGrid.Visibility = Visibility.Collapsed;
+            }
+            else if (tb.Text.ToLower() == "a")
+            {
+                SambaGrid.Visibility = Visibility.Visible;
+                RSZGrid.Visibility = Visibility.Visible;
+                OcaiGrid.Visibility = Visibility.Visible;
+                VansGrid.Visibility = Visibility.Visible;
+
+                DunkGrid.Visibility = Visibility.Collapsed;
+            }
+            else if (tb.Text.ToLower() == "p" || tb.Text.ToLower() == "pu" || tb.Text.ToLower() == "pum" || tb.Text.ToLower() == "puma")
+            {
+                RSZGrid.Visibility = Visibility.Visible;
+
+                OcaiGrid.Visibility = Visibility.Collapsed;
+                VansGrid.Visibility = Visibility.Collapsed;
+                DunkGrid.Visibility = Visibility.Collapsed;
+                SambaGrid.Visibility = Visibility.Collapsed;
+            }
+            else if( tb.Text == "")
+            {
+                VansGrid.Visibility = Visibility.Visible;
+                DunkGrid.Visibility = Visibility.Visible;
+                SambaGrid.Visibility = Visibility.Visible;
+                RSZGrid.Visibility = Visibility.Visible;
+                OcaiGrid.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                VansGrid.Visibility = Visibility.Collapsed;
+                DunkGrid.Visibility = Visibility.Collapsed;
+                SambaGrid.Visibility = Visibility.Collapsed;
+                RSZGrid.Visibility = Visibility.Collapsed;
+                OcaiGrid.Visibility = Visibility.Collapsed;
+            }
         }
 
         private void AddToCart(object sender, RoutedEventArgs e)
